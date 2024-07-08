dbf380a438ca5922c896023ee53a006aa2451350
Bran7astic <128844668+Bran7astic@users.noreply.github.com>
2024-07-08 10:03:21 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+// COUNTDOWN
+
+var launchDate = new Date("Jul 13, 2024 15:30:00").getTime();
+
+const oneSecond = 1000
+const oneMinute = 60 * oneSecond
+const oneHour = 60 * oneMinute
+const oneDay = 24 * oneHour
+
+function addLeadingZero(num) {
+  return num < 10 ? '0'+num : num;
+}
+
+var countdown = setInterval( function () {
+  var currentTime = new Date().getTime();
+  var difference = launchDate - currentTime;
+
+  var days = Math.floor(difference / oneDay);
+  var hours = Math.floor((difference % oneDay) / oneHour);
+  var minutes = Math.floor((difference % oneHour) / oneMinute);
+  var seconds = Math.floor((difference % oneMinute) / oneSecond);
+
+  days = addLeadingZero(days)
+  hours = addLeadingZero(hours)
+  minutes = addLeadingZero(minutes)
+  seconds = addLeadingZero(seconds)
+  
+  document.getElementById("time").textContent = days + " : " + hours + " : " + minutes + " : " + seconds;
+
+  if (difference < 0) {
+    clearInterval(countdown);
+    document.getElementById("time").textContent = "Welcome.";
+  }
+  
+})
+
+// MOUSE MOVEMENT
+
+const background = document.getElementById('background');
+
+function moveBackground(event) {
+  var xPos = event.clientX / window.innerWidth * 40 + 'px';
+  var yPos = -event.clientY / window.innerHeight * 40 + 'px';
+
+  background.style.backgroundPosition = xPos + ' ' + yPos;
+}
+
+document.addEventListener('mousemove', moveBackground)
